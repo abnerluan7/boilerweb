ef4b3a1fa99249cf771c4e7bba8c645b12101de1
Otavio Barros <dev@octasoftware.com.br>
2020-11-20 21:40:58 -0300

    refactor: change useCachedQuery params name

--- src/presentation/hooks/use-cached-query.ts
+++ src/presentation/hooks/use-cached-query.ts
@@ -5,17 +5,17 @@ import { CachedQuery } from '~/presentation/common/protocols'
 export type UseCachedQueryParams<T, R> = {
   queryKey: string
   body?: R
-  callback: () => Promise<T>
+  fetcher: () => Promise<T>
 }
 
 export const useCachedQuery = <T = unknown, R = unknown>({
   queryKey,
-  callback
+  fetcher
 }: UseCachedQueryParams<T, R>): CachedQuery<T> => {
   const { data, error, isFetched, isLoading, isError, isSuccess } = useQuery<
   T,
   ResponseError
-  >(queryKey, callback)
+  >(queryKey, fetcher)
 
   return { error, data, isFetched, isLoading, isError, isSuccess }
 }
